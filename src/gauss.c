9ea4d43403fa0f45c1a82f83a2aac75f1aa09705
jgeb28 <jan-geb28@wp.pl>
2023-01-03 15:09:56 +0100

    Implementacja metody wyboru elementu głównego

--- src/gauss.c
+++ src/gauss.c
@@ -1,5 +1,7 @@
 #include "gauss.h"
 #include "mat_io.h"
+#include <stdlib.h>
+#include <math.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -7,7 +9,26 @@
  */
 int eliminate(Matrix *mat, Matrix *b){
   int n = mat->r;
-  for( int col = 0; col < n-1; col++ ) { // col - przemieszczamy się po kolumnach, n-1 - bez ostatniej
+  for( int col = 0; col < n-1; col++ ) { // col - przemieszczamy się po kolumnach, n-1 bez ostatniej
+    int maxelindex = col; // index elementu maksymalnego
+    double maxel = fabs(mat->data[col][col]); // maxel - element maksymalny
+	for( int row = col+1; row < n; row++) {
+      if(fabs(mat->data[row][col]) > maxel) {  // poszukiwanie elementów większych
+		maxel = mat->data[row][col];
+		maxelindex = row;
+		for( int i= 0; i < n; i++) {                       // zamiana wierszy
+			double *tmp = malloc(n * sizeof (double));
+			tmp[i] = mat->data[col][i];
+			mat->data[col][i] = mat->data[maxelindex][i];
+			mat->data[maxelindex][i]= tmp[i];
+			free(tmp);
+		}
+		double tmp2;
+			tmp2 = b->data[col][0];
+			b->data[col][0] = b->data[maxelindex][0];
+			b->data[maxelindex][0] = tmp2;
+	  }
+	 }
   	 for( int row = col+1; row < n; row++){ // row - przemieszczamy się po wierszach
        if( mat->data[col][col] == 0)
 	     return 1;
@@ -17,7 +38,5 @@ int eliminate(Matrix *mat, Matrix *b){
 	   b->data[row][0] -= ratio*b->data[col][0];
 	 } 
   }
-  printToScreen(mat);
-  printToScreen(b);
 return 0;
 }
