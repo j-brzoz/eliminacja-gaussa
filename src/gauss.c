b7a611d1ebcfc59b82c53a714ccb4741f33d907d
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2023-01-03 15:29:08 +0100

    rozwiazanie konfliktu

--- src/gauss.c
+++ src/gauss.c
@@ -9,31 +9,7 @@
  */
 int eliminate(Matrix *mat, Matrix *b){
   int n = mat->r;
-<<<<<<< HEAD
-  for( int col = 0; col < n-1; col++ ) { // col - przemieszczamy się po colch, n-1 bez ostatniej
- 	double maxWartosc = fabs(mat->data[col][col]); 
-	int maxWartoscIndex = col; 
-	
-	for( int row = col+1; row < n; row++) {
-      if(fabs(mat->data[row][col]) > maxWartosc) {
-		maxWartoscIndex = row;
-		maxWartosc = mat->data[row][col];
-		for( int i= 0; i < n; i++) {
-			double *tmpMat = malloc(n * sizeof (double));
-			tmpMat[i] = mat->data[col][i];
-			mat->data[col][i] = mat->data[maxWartoscIndex][i];
-			mat->data[maxWartoscIndex][i]= tmpMat[i];
-			free(tmpMat);
-		}
-		double tmpB;
-		tmpB = b->data[col][0];
-		b->data[col][0] = b->data[maxWartoscIndex][0]; 
-		b->data[maxWartoscIndex][0] = tmpB;
-	  }
-	}
-	 
 
-=======
   for( int col = 0; col < n-1; col++ ) { // col - przemieszczamy się po kolumnach, n-1 bez ostatniej
     int maxelindex = col; // index elementu maksymalnego
     double maxel = fabs(mat->data[col][col]); // maxel - element maksymalny
@@ -54,7 +30,7 @@ int eliminate(Matrix *mat, Matrix *b){
 			b->data[maxelindex][0] = tmp2;
 	  }
 	 }
->>>>>>> 9ea4d43403fa0f45c1a82f83a2aac75f1aa09705
+
   	 for( int row = col+1; row < n; row++){ // row - przemieszczamy się po wierszach
        if( mat->data[col][col] == 0)
 	     return 1;
