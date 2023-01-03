c9b6176583ca35e721df0ca048116c743411a379
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2023-01-03 15:11:58 +0100

    implementacja metody wyboru elementu glownego

--- src/gauss.c
+++ src/gauss.c
@@ -1,5 +1,7 @@
 #include "gauss.h"
 #include "mat_io.h"
+#include <stdlib.h>
+#include <math.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -7,17 +9,38 @@
  */
 int eliminate(Matrix *mat, Matrix *b){
   int n = mat->r;
-  for( int col = 0; col < n-1; col++ ) { // col - przemieszczamy się po kolumnach, n-1 - bez ostatniej
+  for( int col = 0; col < n-1; col++ ) { // col - przemieszczamy się po colch, n-1 bez ostatniej
+ 	double maxWartosc = fabs(mat->data[col][col]); 
+	int maxWartoscIndex = col; 
+	
+	for( int row = col+1; row < n; row++) {
+      if(fabs(mat->data[row][col]) > maxWartosc) {
+		maxWartoscIndex = row;
+		maxWartosc = mat->data[row][col];
+		for( int i= 0; i < n; i++) {
+			double *tmpMat = malloc(n * sizeof (double));
+			tmpMat[i] = mat->data[col][i];
+			mat->data[col][i] = mat->data[maxWartoscIndex][i];
+			mat->data[maxWartoscIndex][i]= tmpMat[i];
+			free(tmpMat);
+		}
+		double tmpB;
+		tmpB = b->data[col][0];
+		b->data[col][0] = b->data[maxWartoscIndex][0]; 
+		b->data[maxWartoscIndex][0] = tmpB;
+	  }
+	}
+	 
+
   	 for( int row = col+1; row < n; row++){ // row - przemieszczamy się po wierszach
        if( mat->data[col][col] == 0)
 	     return 1;
-	   double ratio = mat->data[row][col] / mat->data[col][col]; // colsub - przemieszczenie po kolumnach celem substrakcji
+	   double ratio = mat->data[row][col] / mat->data[col][col]; // colsub - przemieszczenie po colch celem substrakcji
 	   for( int colsub = 0; colsub < n; colsub++)
 		 mat->data[row][colsub] -= ratio * mat->data[col][colsub];
 	   b->data[row][0] -= ratio*b->data[col][0];
 	 } 
+  	
   }
-  printToScreen(mat);
-  printToScreen(b);
 return 0;
 }
